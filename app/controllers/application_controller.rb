9db409068f40b0afbb16c4f223081dc3809e6cc1
Stefan Buhrmester <buhrmi@gmail.com>
2016-02-04 21:18:39 +0900

    layout design

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,7 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :set_locale
   before_filter :allow_me
 
   private
@@ -32,6 +33,18 @@ class ApplicationController < ActionController::Base
 
   def after_sign_in_path_for(resource)
     dashboard_path
+  end
+
+  def set_locale
+    if params[:locale]
+      I18n.locale = cookies.permanent[:locale] = params[:locale]
+    elsif current_user
+      # TODO: save/get locale in user
+    elsif cookies[:locale]
+      I18n.locale = cookies[:locale]
+    else
+      I18n.locale = I18n.default_locale # TODO: take locale from geoip
+    end
   end
 
 end
