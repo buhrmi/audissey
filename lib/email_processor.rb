6903cb902759ff80b9f77056ae24e530639d9a13
Stefan Buhrmester <buhrmi@gmail.com>
2016-02-11 14:38:04 +0900

    its a hash

--- lib/email_processor.rb
+++ lib/email_processor.rb
@@ -6,7 +6,7 @@ class EmailProcessor
   def process
     email = @email
     from = email.from
-    encrypted = email.to.first.email.to_s.gsub('reply+', '').gsub('@relay.audissey.fm','')
+    encrypted = email.to.first[:email].to_s.gsub('reply+', '').gsub('@relay.audissey.fm','')
     crypt = ActiveSupport::MessageEncryptor.new(ENV['SECRET_KEY_BASE'])
     decrypted = crypt.decrypt_and_verify(encrypted)
     data = JSON.parse(decrypted)
