dfdb70a4daf3ce65d49306bbd61ddc4c5330efbe
Stefan Buhrmester <buhrmi@gmail.com>
2016-02-16 08:55:01 +0900

    locales stuff

--- app/models/category.rb
+++ app/models/category.rb
@@ -17,6 +17,7 @@ class Category < ActiveRecord::Base
   find_or_create_by :id => 'venue',         :name => 'Venue'
   find_or_create_by :id => 'organizer',     :name => 'Organizer'
   find_or_create_by :id => 'photographer',     :name => 'Photographer'
+  find_or_create_by :id => 'catering',     :name => 'Catering'
 
   has_many :offerings
 
