aefd54c14c28b2f4caf791650891a9ff4922e432
Stefan Buhrmester <buhrmi@gmail.com>
2016-02-22 19:55:31 +0900

    gallery management

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -18,7 +18,17 @@
 class UploadsController < ApplicationController
   before_filter :authenticate_user!
   include ActionView::Helpers::NumberHelper # ugly. use jsonbuilder
-
+  include ActionView::RecordIdentifier
+  
+  def destroy
+    upload = Upload.find params[:id]
+    return cancel unless upload.user_id == current_user.id
+    upload.destroy
+    render :js do
+      jQuery('#'+dom_id(upload)).remove!
+    end
+  end
+  
   def index
     if params[:type] == 'image'
       files = current_user.uploads.images.limit(10).order('created_at DESC')
@@ -57,7 +67,9 @@ class UploadsController < ApplicationController
       :user_id => current_user.id, :mime_type => file_param.content_type, :through => params[:through],
       :attachable_id => params[:attachable_id], :attachable_type => params[:attachable_type]
 
-
+    attachable_dom_id = params[:attachable_type]+'_'+params[:attachable_id]
+    attachable_dom_id.downcase!
+    
     if params[:through] == 'redactor'
       render :json => {'filelink' => upload.file.url}
     else
@@ -68,7 +80,10 @@ class UploadsController < ApplicationController
       respond_to do |format|
         format.html { render action: 'new', layout: false }
         format.json do
-          render :json => { :url => upload.file.url }
+          # render :json => { :url => upload.file.url }
+          render :js do 
+            jQuery(render :partial => 'uploads/upload', :locals => {:upload => upload, :editable => true}, :formats => [:html]).appendTo('#uploads_'+attachable_dom_id)
+          end
         end
       end
     end
