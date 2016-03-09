dfd6bcfcd07b6b9d77ce9dceb019f68882f9da41
Stefan Buhrmester <buhrmi@gmail.com>
2016-03-09 21:32:48 +0900

    add rentals

--- app/models/category.rb
+++ app/models/category.rb
@@ -18,7 +18,7 @@ class Category < ActiveRecord::Base
   find_or_create_by :id => 'venue', :name => 'Venue'
   find_or_create_by :id => 'photographer', :name => 'Photographer'
   find_or_create_by :id => 'catering', :name => 'Catering'
-  find_or_create_by :id => 'consultant', :name => 'Consultant'
+  find_or_create_by :id => 'other', :name => 'Other'
   find_or_create_by :id => 'model', :name => 'Model / Dancer'
   find_or_create_by :id => 'rental', :name => 'Rental Gear'
 
